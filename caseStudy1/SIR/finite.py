7c7bc42022e5813e8b4c4d43a2f503454a644afb
zellaa <zellbaig@gmail.com>
2022-11-16 22:02:35 +0000

    Working with time components! Still need to fiddle with space.

--- caseStudy1/SIR/finite.py
+++ caseStudy1/SIR/finite.py
@@ -9,24 +9,24 @@ rho = gamma/beta
 deathRate = 30/100000
 alpha = deathRate/beta
 
-N = 10000000
+N0 = 100000
 i0, r0 = 100, 0
-s0 = N-i0-r0
-s0Hat, i0Hat, r0Hat, d0Hat = (s0/N), (i0/N), (r0/N), 0
+s0 = N0-i0-r0
+s0Hat, i0Hat, r0Hat, d0Hat = (s0/N0), (i0/N0), (r0/N0), 0
 y0Hat = s0Hat, i0Hat, r0Hat, d0Hat
 ##
 
 #finalTime = 10
-finalTime = 1*beta
+finalTime = 20
 a = 0
 b = 2
 finalSpace = b-a
-deltaT = 1e-8
-deltaX = 1
+deltaT = 0.001
+deltaX = 0.01
 J = int(round(finalSpace/deltaX))
 N = int(round(finalTime/deltaT))
 r = 0.02
-p = 0
+p = 0.5
 
 ##
 # Placeholder function to generate initial data
@@ -54,33 +54,33 @@ def propagateSIR(S,I,R,N):
         rUpdate = R[:][i] + (rho*I[:][i])*deltaT
         S[:][i+1] = sUpdate
         I[:][i+1] = iUpdate
-        S[:][i+1] = rUpdate
+        R[:][i+1] = rUpdate
     return S,I,R
 ##
 S,I,R = initialiseArrays()
 ##
 S,I,R = propagateSIR(S,I,R,N)
 ##
-tValues = np.linspace(0,finalTime,num=N)
+tValues = np.linspace(0,finalTime,num=N)/beta
 xValues = np.linspace(0,finalSpace,num=J)
 X,T = np.meshgrid(xValues,tValues)
 fig = plt.figure(figsize=plt.figaspect(0.35))
 ax1 = fig.add_subplot(1,3,1,projection='3d')
-ax1.contour3D(X,T,S,50)
+ax1.contour3D(X,T,S*N0,50)
 ax1.set_xlabel('Position in Space')
 ax1.set_ylabel('Time')
 ax1.set_zlabel('Susceptibles')
 ax1.view_init(20, 135)
 
 ax2 = fig.add_subplot(1,3,2,projection='3d')
-ax2.contour3D(X,T,I,50)
+ax2.contour3D(X,T,I*N0,50)
 ax2.view_init(20, 135)
 ax2.set_xlabel('Position in Space')
 ax2.set_ylabel('Time')
 ax2.set_zlabel('Infected')
 
 ax3 = fig.add_subplot(1,3,3,projection='3d')
-ax3.contour3D(X,T,R,50)
+ax3.contour3D(X,T,R*N0,50)
 ax3.view_init(20, 135)
 ax3.set_xlabel('Position in Space')
 ax3.set_ylabel('Time')
