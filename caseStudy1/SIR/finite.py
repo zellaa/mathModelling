12085f2c8f201bd250b8e2bdc7fc8a874147ba04
zellaa <zellbaig@gmail.com>
2022-11-16 22:14:12 +0000

    Working with a very basic model - some proportion of the people in an area are infected, and they disperse over time. We see that infected cases begin to rise in neighbouring areas.

--- caseStudy1/SIR/finite.py
+++ caseStudy1/SIR/finite.py
@@ -19,14 +19,14 @@ y0Hat = s0Hat, i0Hat, r0Hat, d0Hat
 #finalTime = 10
 finalTime = 20
 a = 0
-b = 2
+b = 4
 finalSpace = b-a
 deltaT = 0.001
 deltaX = 0.01
 J = int(round(finalSpace/deltaX))
 N = int(round(finalTime/deltaT))
 r = 0.02
-p = 0.5
+p = 0.05
 
 ##
 # Placeholder function to generate initial data
@@ -39,7 +39,7 @@ def initialiseArrays():
     I = np.reshape(I,(len(I),int(N))).transpose()
     R = np.reshape(R,(len(R),int(N))).transpose()
 
-    S[0] = s0Hat
+    S[0][100:200] = s0Hat
     I[0] = i0Hat
     R[0] = r0Hat
     return S,I,R
@@ -61,26 +61,27 @@ S,I,R = initialiseArrays()
 ##
 S,I,R = propagateSIR(S,I,R,N)
 ##
+gridDensity=200
 tValues = np.linspace(0,finalTime,num=N)/beta
 xValues = np.linspace(0,finalSpace,num=J)
 X,T = np.meshgrid(xValues,tValues)
 fig = plt.figure(figsize=plt.figaspect(0.35))
 ax1 = fig.add_subplot(1,3,1,projection='3d')
-ax1.contour3D(X,T,S*N0,50)
+ax1.contour3D(X,T,S*N0,gridDensity)
 ax1.set_xlabel('Position in Space')
 ax1.set_ylabel('Time')
 ax1.set_zlabel('Susceptibles')
 ax1.view_init(20, 135)
 
 ax2 = fig.add_subplot(1,3,2,projection='3d')
-ax2.contour3D(X,T,I*N0,50)
+ax2.contour3D(X,T,I*N0,gridDensity)
 ax2.view_init(20, 135)
 ax2.set_xlabel('Position in Space')
 ax2.set_ylabel('Time')
 ax2.set_zlabel('Infected')
 
 ax3 = fig.add_subplot(1,3,3,projection='3d')
-ax3.contour3D(X,T,R*N0,50)
+ax3.contour3D(X,T,R*N0,gridDensity)
 ax3.view_init(20, 135)
 ax3.set_xlabel('Position in Space')
 ax3.set_ylabel('Time')
